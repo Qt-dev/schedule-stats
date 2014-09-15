c5386ebdf0b661ff5af4f3220a16b179ea750873
Quentin Devauchelle <quentin@devauchelle.eu>
2014-09-15 16:00:19 +0200

    Add calendar event list feature
    Add a new function that takes the event list from gCal and then gives it back to the view.

--- app/controllers/pages.js
+++ app/controllers/pages.js
@@ -1,5 +1,29 @@
 var now = new Date();
+var calendar = require('googleapis').calendar('v3');
+
+var eventList = [];
+
+var getCurrentMonthEventList = function(done){
+  var timeMin = helpers.time.getFirstDayOfMonth(now);
+  var timeMax = helpers.time.getLastDayOfMonth(now);
+
+  calendar.events.list({
+    auth: KEYS.API,
+    calendarId: KEYS.CALENDAR,
+    timeMin: timeMin.toISOString(),
+    timeMax: timeMax.toISOString()
+  }, function(err, list){
+    if(err){
+      console.log(err);
+      done();
+    }
+
+    done(list.items);
+  })
+}
 
 exports.index = function(req, res){
-  res.render('pages/index');
+  getCurrentMonthEventList(function(events){
+    res.render('pages/index', {events: events});
+  });
 }
