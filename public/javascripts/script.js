ff612fe4610090acd1c42f85e250db50f4601649
Quentin Devauchelle <quentin@devauchelle.eu>
2014-09-20 17:26:23 +0200

    Add a controller
    It creates groups, from the model,
    then all the groups get their own heights, bind click on title,
    and close themselves

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,26 +1,61 @@
+var App = {};
+var Selectors = {
+  group: ".group",
+  title: ".group > h2",
+  content: ".group > ul"
+}
 $(document).ready(function(){
-  
+  App.controller = new Controller(Selectors);
 });
 
 // GROUP MODEL
 var Group = function(el){
-  this.closed = true;
   this.el = el;
+  this.init();
+  this.closeContent();
 }
 Group.prototype = {
-  open: function(){
-    this.el.children('ul').height = initial;
+  init: function(){
+    this.content = $(this.el).children('ul');
+    this.title = $(this.el).children('h2');
+    this.contentHeight = this.content.outerHeight();
+    this.bindClickToOpen(); 
+  },
+  openContent: function(){
+    this.content.height(this.contentHeight);
     this.closed = false;
   },
-  close: function(){
-    this.el.children('ul').height = 0;
+  closeContent: function(){
+    this.content.height("0");
     this.closed = true;
   },
   toggle: function(){
     if(this.closed){
-      this.open();
+      this.openContent();
     }else{
-      this.close();
+      this.closeContent();
     }
+  },
+  bindClickToOpen: function(){
+    var toggle = this.toggle;
+    this.title.on('click', function(e){
+      this.toggle();
+    }.bind(this));
+  }
+}
+
+
+// CONTROLLER
+var Controller = function(selectors){
+  this.selectors = selectors;
+  this.init();
+}
+Controller.prototype = {
+  init: function(){
+    var groups = [];
+    $(this.selectors.group).each(function(group){
+      groups.push(new Group(this));
+    })
+    this.groups = groups;
   }
 }
